5e7cd32de85e02133e2acc43140742fb4db89eba
lihao <romcdy@163.com>
2017-10-30 19:05:21 +0800

    update

--- db_fixture/mysql_db.py
+++ db_fixture/mysql_db.py
@@ -18,3 +18,54 @@ port = cf.get("mysqlconf", "port")
 db = cf.get("mysqlconf", "db_name")
 user = cf.get("mysqlconf", "user")
 password = cf.get("mysqlconf", "password")
+
+#===========封装MySQL基本操作===============
+class DB:
+    def __init__(self):
+        try:
+            #连接数据库
+            self.conn = connect(host=host,
+                                user=user,
+                                password=password,
+                                db=db,
+                                charset='utf8mb4',
+                                cursorclass=cursors.DictCursor)
+        except OperationalError as e:
+            print("mysql error %d: %s" % (e.args[0], e.args[1]))
+
+    #清楚表数据
+    def clear(self, table_name):
+    #real_sql = "truncate table" + table_name + ";"
+        real_sql = "delete from" + table_name + ";"
+        with self.conn.cursor() as cursor:
+            cursor.execute("SET FOREIGN_KEY_CHECKS=0;")
+            cursor.execute(real_sql)
+        self.conn.commit()
+
+    #插入表数据库
+    def insert(self, table_name, table_data):
+        for key in table_data:
+            table_data[key] = "'" + str(table_data[key])+ "'"
+        key = ','.join(table_data.keys())
+        value = ','.join(table_data.values())
+        real_sql = "INSERT INTO" + table_name + "("+key+") VALUES (" + value + ")"
+        #print(real_sql)
+
+        with self.conn.cursor() as cursor:
+            cursor.execute(real_sql)
+        self.conn.commit()
+
+    #关闭数据库连接
+    def close(self):
+        self.conn.close()
+
+if __name__ == '__main__':
+    db = DB()
+    table_name = "sign_event"
+    data = {'id': 1, 'name': '红米', '`limit`': 2000, 'status': 1, 'address': '北京会展中心', 'start_time':'2017-10-30 00:25:42'}
+    table_name2 = "sign_guest"
+    data2 = {'realname': 'alen', 'phone': 12312341234, 'email': 'alen@mail.com', 'sign': 0, 'event_id': 1}
+
+    db.clear(table_name)
+    db.insert(table_name, data)
+    db.close()
